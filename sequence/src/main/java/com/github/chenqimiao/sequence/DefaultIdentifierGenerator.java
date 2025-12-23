55ae2afb7aaaeb876a7ec25986815ccd5e3f42ae
Qimiao Chen <chenqimiao1994@126.com>
2025-12-23 14:58:06 +0800

    Remove unnecessary blank lines

--- sequence/src/main/java/com/github/chenqimiao/sequence/DefaultIdentifierGenerator.java
+++ sequence/src/main/java/com/github/chenqimiao/sequence/DefaultIdentifierGenerator.java
@@ -8,17 +8,15 @@ import io.github.mocreates.config.DefaultSequenceConfig;
  * @date 2022-10-12 11:25
  **/
 public class DefaultIdentifierGenerator implements IdentifierGenerator{
+
     private final Sequence sequence;
-
-
 
     public DefaultIdentifierGenerator(long workerId) {
         DefaultSequenceConfig defaultSequenceConfig = new DefaultSequenceConfig();
         defaultSequenceConfig.setWorkerId(workerId);
         sequence =  new Sequence(defaultSequenceConfig);
     }
-
-
+    
     @Override
     public Long nextId() {
         return sequence.nextId();
