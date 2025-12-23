9ac7acffa28ec1556c8c2c5c3c9e2c7430c7123e
Qimiao Chen <chenqimiao1994@126.com>
2025-12-23 14:58:23 +0800

    Feature/upgrade (#3)
    * upgrade
    
    * upgrade version
    
    * Remove unnecessary blank lines

--- sequence/src/main/java/com/github/chenqimiao/sequence/DefaultIdentifierGenerator.java
+++ sequence/src/main/java/com/github/chenqimiao/sequence/DefaultIdentifierGenerator.java
@@ -1,30 +1,22 @@
 package com.github.chenqimiao.sequence;
 
-import java.net.InetAddress;
+import io.github.mocreates.Sequence;
+import io.github.mocreates.config.DefaultSequenceConfig;
 
 /**
  * @author Qimiao Chen
  * @date 2022-10-12 11:25
  **/
 public class DefaultIdentifierGenerator implements IdentifierGenerator{
+
     private final Sequence sequence;
 
-    public DefaultIdentifierGenerator() {
-        this.sequence = new Sequence(null);
+    public DefaultIdentifierGenerator(long workerId) {
+        DefaultSequenceConfig defaultSequenceConfig = new DefaultSequenceConfig();
+        defaultSequenceConfig.setWorkerId(workerId);
+        sequence =  new Sequence(defaultSequenceConfig);
     }
-
-    public DefaultIdentifierGenerator(InetAddress inetAddress) {
-        this.sequence = new Sequence(inetAddress);
-    }
-
-    public DefaultIdentifierGenerator(long workerId, long dataCenterId) {
-        this.sequence = new Sequence(workerId, dataCenterId);
-    }
-
-    public DefaultIdentifierGenerator(Sequence sequence) {
-        this.sequence = sequence;
-    }
-
+    
     @Override
     public Long nextId() {
         return sequence.nextId();
